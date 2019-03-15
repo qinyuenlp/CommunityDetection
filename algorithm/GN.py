be0501eb26127ebe8c5ec2a6200956f828553947
Qin <39150772+QinY-Stat@users.noreply.github.com>
2019-03-16 01:37:38 +0800

    Add files via upload

---
+++ algorithm/GN.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+'''
+Girvan M, Newman M E J. Community structure in social and biological networks[J]. Proceedings of the National Academy of
+ the United States of America, 2002, 99(12): 7821-7826.
+'''
+import networkx as nx
+import community as cm
+
+import copy
+
+# class GN:
+#     def __init__(self, G):
+#         self._G = copy.deepcopy(G)
+#         self._G0 = G
+#         self._num_edges = G.number_of_edges()
+
+def all_results(G):
+    results = []
+    m = G.number_of_edges()
+    for i in range(m):
+        betweenness_dict = nx.edge_betweenness_centrality(G)
+        edge_max_betweenness = max(betweenness_dict.items(), key=lambda x:x[1])[0]
+        G.remove_edge(edge_max_betweenness[0], edge_max_betweenness[1])
+        community = [list(subgraph) for subgraph in list(nx.connected_component_subgraphs(G))]
+        community_dict = {node:0 for node in G.nodes()}
+        for i in range(len(community)):
+            each = community[i]
+            for node in each:
+                community_dict[node] = i
+        results.append(community_dict)
+    return results
+
+def partition(G):
+    G_copy = copy.deepcopy(G)
+    results = all_results(G)
+    modularities = [cm.modularity(results[i], G_copy) for i in range(len(results))]
+    max_modularity = max(modularities)
+    max_index = modularities.index(max_modularity)
+    max_result = results[max_index]
+    return list(max_result.values()), max_modularity
+
+if __name__ == '__main__':
+
+    filepath = r'.\LoadData.gml'
+
+    G = nx.read_gml(filepath)
+    community, modularity = partition(G)
+    print(community)
+
+
+
+
+
---
+++ algorithm/SpectralClustering.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+'''
+参考文献:
+Luxburg U V. A tutorial on spectral clustering[J]. Statistics and Computing, 2007, 17(4): 395-416
+
+参考博客:
+https://blog.csdn.net/waleking/article/details/7584084
+'''
+import networkx as nx
+import numpy as np
+from sklearn.cluster import KMeans
+import scipy.linalg as linalg
+
+# class SpectralClustering:
+#     def __init__(self, G, k):
+#         self.G = G
+#         self.k = k
+
+def partition(G, k, normalized=False):
+    A = nx.to_numpy_array(G)
+    D = degree_matrix(G)
+    L = D - A
+    Dn = np.power(np.linalg.matrix_power(D, -1), 0.5)
+    L = np.dot(np.dot(Dn, L), Dn)
+    if normalized:
+        pass
+    eigvals, eigvecs = linalg.eig(L)
+    n = len(eigvals)
+
+    dict_eigvals = dict(zip(eigvals, range(0, n)))
+    k_eigvals = np.sort(eigvals)[0:k]
+    eigval_indexs = [dict_eigvals[k] for k in k_eigvals]
+    k_eigvecs = eigvecs[:, eigval_indexs]
+    result = KMeans(n_clusters=k).fit_predict(k_eigvecs)
+    return result
+
+def degree_matrix(G):
+    n = G.number_of_nodes()
+    V = [node for node in G.nodes()]
+    D = np.zeros((n, n))
+    for i in range(n):
+        node = V[i]
+        d_node = G.degree(node)
+        D[i][i] = d_node
+    return np.array(D)
+
+
+if __name__ == '__main__':
+
+    filepath = r'.\LoadData.gml'
+
+    G = nx.read_gml(filepath)
+    k = 9
+    a = partition(G, k)
+    b = a.partition()
+    print(b)
+
+
+
